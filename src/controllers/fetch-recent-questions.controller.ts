c6114fc32c927c2ecb7a4a185bddbd8a4b1edbe6
hidelrios <hidels.rios@gmail.com>
2024-10-24 22:24:12 -0300

    feat: testes e2e de perguntas

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -13,11 +13,12 @@ const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
 type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
+
 export class FetchRecentQuestionsController {
   constructor(private prisma: PrismaService) { }
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const perPage = 1
+    const perPage = 20
     const questions = await this.prisma.question.findMany({
       take: perPage,
       skip: (page - 1) * perPage,
